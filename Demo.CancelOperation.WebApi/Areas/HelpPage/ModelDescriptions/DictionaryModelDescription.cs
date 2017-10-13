ae94cc04ce029b9b00bc7be3d573b84090a51066
Anthony Sneed <tony@tonysneed.com>
2017-10-12 19:32:53 -0500

    Add project files.

---
+++ Demo.CancelOperation.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Demo.CancelOperation.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Demo.CancelOperation.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Demo.CancelOperation.WebApi.WebApiApplication" Language="C#" %>
